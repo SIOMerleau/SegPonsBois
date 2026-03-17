00a79682c5ee0b854d2ada1295e7e808cdafb499
Beuscart Clément <clement.beuscart@lycee-merleauponty.fr>
2026-03-17 10:29:56 +0100

    Add files via upload

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -2,38 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\VisiteurRequest;
 use App\Models\Visiteur;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class VisiteurController extends Controller
 {
-    //
-    public function create() {
-        // On retourne la vue contact
-        return view('contact');
+    public function index()
+    {
+        return Visiteur::all();
     }
 
-    public function store(VisiteurRequest $request) {
+    public function store(Request $request)
+    {
+        return Visiteur::create($request->all());
+    }
 
-        try{
-        // Les données validées sont disponibles via $request->validated()
-        $validatedData = $request->validated();
-        //Ajout de la date du moment du contact
-        $validatedData['dateContact'] = date('Y-m-d');
+    public function show(Visiteur $visiteur)
+    {
+        return $visiteur;
+    }
 
-        // On crée un visiteur avec les données validées
-        $visiteur = Visiteur::create($validatedData);
+    public function update(Request $request, Visiteur $visiteur)
+    {
+        $visiteur->update($request->all());
+        return $visiteur;
+    }
 
-        //Log
-        Log::info('Message contact envoyé par : '. $visiteur->nomVisiteur);
-        // On redirige vers la page de contact avec un message de succès
-        return redirect()->route('contact.create')->with('success', 'Message envoyé avec succès.');
-        } catch (\Exception $e) {
-            //Log
-            Log::error('Erreur lors de l\'envoi du message de '. $request->nomVisiteur .': ' . $e->getMessage());
-            // On redirige vers la page de contact avec un message d'erreur
-            return redirect()->route('contact.create')->with('error', 'Une erreur est survenue lors de l\'envoi du message.');
-        }
+    public function destroy(Visiteur $visiteur)
+    {
+        $visiteur->delete();
+        return response()->json(null, 204);
     }
 }
+
