e51981bc5151f7e705a00a52987009b4b9b7f56d
Perrin Erwan <erwanperrin51@gmail.com>
2026-03-17 13:22:38 +0100

    Delete resources directory

--- resources/views/partials/work-in-progress.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- resources/views/partials/work-in-progress.blade.php -->
-<div class="h-fit bg-blue-500">
-    <p class="underline text-white text-center">Ceci est une page non terminé</p>
-</div>
