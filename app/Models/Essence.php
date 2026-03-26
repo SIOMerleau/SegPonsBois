97ecfd8454727a0653e04a5770a9142d4d3fd920
Beuscart Clément <clement.beuscart@lycee-merleauponty.fr>
2026-03-26 15:49:18 +0100

    bbb

--- app/Models/Essence.php
+++ app/Models/Essence.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Essence extends Model
 {
     protected $fillable = ['idEssence', 'varieteEssence', 'typeEssence', 'prixEssence', 'nomLatinEssence', 'descriptionEssence','origineEssence', 'densiteEssence', 'durabiliteEssence', 'commentaireEssence', 'photoEssence'];
-}
+
+    protected $table = 'essence';
+    }
