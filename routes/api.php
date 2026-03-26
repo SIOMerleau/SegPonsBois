52695e86f631a754ff19dd2e8c1b70ccd44b64fc
Beuscart Clément <clement.beuscart@lycee-merleauponty.fr>
2026-03-26 16:40:12 +0100

    modif api2

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EssenceController;
-use App\Http\Controllers\VisiteurController;
-use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\ProduitController ;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
