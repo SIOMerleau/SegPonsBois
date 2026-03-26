80466ec1912f63982ea1e729ee9848e1e6d4acd9
Beuscart Clément <clement.beuscart@lycee-merleauponty.fr>
2026-03-26 16:09:02 +0100

    modification

--- app/Http/Controllers/EssenceController.php
+++ app/Http/Controllers/EssenceController.php
@@ -10,22 +10,34 @@ use App\Http\Controllers\Controller;
 
 class EssenceController extends Controller
 {
-    
-  public function index(){
+
+    public function index()
+    {
 
         return Essence::all();
     }
 
 
-  public function store(Request $request){Essence::create($request->all()); }
-
-  
-  public function show(Essence $id){
-      return $id;}
-
-
-  public function update(Request $request, Essence $id){$id->update($request->all());}
+    public function store(Request $request)
+    {
+        Essence::create($request->all()); 
+    }
 
 
-  public function destroy(Essence $id){$id->delete();}
+    public function show(Essence $id)
+    {
+        return $id;
+    }
+
+
+    public function update(Request $request, Essence $id)
+    {
+        $id->update($request->all());
+    }
+
+
+    public function destroy(Essence $id)
+    {
+        $id->delete();
+    }
 }
